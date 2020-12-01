875812e94a69a2ad6c20a6ea3700696e738a6885
Jairo <jairogarciachaves@gmail.com>
2020-12-01 15:25:34 +0100

    Añadido PUBLIC

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
+//cargar el express
 const express = require("express");
+
+//cargar el modulo handlebars
 const exphbs = require("express-handlebars");
+
+//crea un servidor utilizando express
 const app = express();
+
+//elige el puerto en el que se montara el servidor
 const port = 3000;
 
+//escucha el puerto
+app.listen(port);
 
+
+//elige el motor de renderizado
 app.set("view engine", "handlebars");
+
+//configuración del motor
 app.engine("handlebars", exphbs());
+
+//indica el lugar donde coge los "parciales"
 app.get("/", function(req, res){
     res.render("index");
 });
-app.listen(port);
+
+app.use(express.static("public"));
